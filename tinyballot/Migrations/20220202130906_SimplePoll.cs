97d443d25766f856c6e5d95b62ac64d8eaac462f
Felix Yowtang Liu <9469548+ghalox@users.noreply.github.com>
2022-02-02 10:43:08 -0300

    Add EF migrations for SimplePoll.

---
+++ tinyballot/Migrations/20220202130906_SimplePoll.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tinyballot.Migrations
+{
+    public partial class SimplePoll : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Polls",
+                columns: table => new
+                {
+                    PollId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polls", x => x.PollId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ballots",
+                columns: table => new
+                {
+                    BallotId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PollId = table.Column<int>(type: "int", nullable: false),
+                    Voter = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ballots", x => x.BallotId);
+                    table.ForeignKey(
+                        name: "FK_Ballots_Polls_PollId",
+                        column: x => x.PollId,
+                        principalTable: "Polls",
+                        principalColumn: "PollId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    CandidateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PollId = table.Column<int>(type: "int", nullable: false),
+                    Label = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.CandidateId);
+                    table.ForeignKey(
+                        name: "FK_Candidates_Polls_PollId",
+                        column: x => x.PollId,
+                        principalTable: "Polls",
+                        principalColumn: "PollId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BallotCandidates",
+                columns: table => new
+                {
+                    BallotId = table.Column<int>(type: "int", nullable: false),
+                    CandidateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BallotCandidates", x => new { x.BallotId, x.CandidateId });
+                    table.ForeignKey(
+                        name: "FK_BallotCandidates_Ballots_BallotId",
+                        column: x => x.BallotId,
+                        principalTable: "Ballots",
+                        principalColumn: "BallotId");
+                    table.ForeignKey(
+                        name: "FK_BallotCandidates_Candidates_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidates",
+                        principalColumn: "CandidateId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BallotCandidates_CandidateId",
+                table: "BallotCandidates",
+                column: "CandidateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ballots_PollId",
+                table: "Ballots",
+                column: "PollId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_PollId",
+                table: "Candidates",
+                column: "PollId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BallotCandidates");
+
+            migrationBuilder.DropTable(
+                name: "Ballots");
+
+            migrationBuilder.DropTable(
+                name: "Candidates");
+
+            migrationBuilder.DropTable(
+                name: "Polls");
+        }
+    }
+}
