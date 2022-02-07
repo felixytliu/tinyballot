3dc84c79f54751dee75062315295708783a3d782
Felix Yowtang Liu <9469548+ghalox@users.noreply.github.com>
2022-02-07 12:37:10 -0300

    Remove explicit OnDelete configuration for BallotCandidate.

--- tinyballot/Data/SimplePollContext.cs
+++ tinyballot/Data/SimplePollContext.cs
@@ -8,26 +8,24 @@ namespace TinyBallot.Data;
 public class SimplePollContext : DbContext
 {
     public SimplePollContext(DbContextOptions<SimplePollContext> options)
-	: base(options) {}
+        : base(options) {}
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-	modelBuilder.Entity<BallotCandidate>()
-	    .HasKey(bc => new { bc.BallotId, bc.CandidateId });
-
-	modelBuilder.Entity<BallotCandidate>()
-	    .HasOne<Ballot>(bc => bc.Ballot)
-	    .WithMany(b => b.BallotCandidates)
-	    .HasForeignKey(bc => bc.BallotId)
-	    .OnDelete(DeleteBehavior.ClientCascade);
-
-	modelBuilder.Entity<BallotCandidate>()
-	    .HasOne<Candidate>(bc => bc.Candidate)
-	    .WithMany(c => c.BallotCandidates)
-	    .HasForeignKey(bc => bc.CandidateId)
-            .OnDelete(DeleteBehavior.ClientCascade);
+        modelBuilder.Entity<BallotCandidate>()
+            .HasKey(bc => new { bc.BallotId, bc.CandidateId });
+        
+        modelBuilder.Entity<BallotCandidate>()
+            .HasOne<Ballot>(bc => bc.Ballot)
+            .WithMany(b => b.BallotCandidates)
+            .HasForeignKey(bc => bc.BallotId);
+        
+        modelBuilder.Entity<BallotCandidate>()
+            .HasOne<Candidate>(bc => bc.Candidate)
+            .WithMany(c => c.BallotCandidates)
+            .HasForeignKey(bc => bc.CandidateId);
     }
-
+    
     public DbSet<Poll> Polls { get; set; }
     public DbSet<Ballot> Ballots { get; set; }
     public DbSet<Candidate> Candidates { get; set; }
